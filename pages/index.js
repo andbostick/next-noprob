44ee8e473fe697537297d230d72a79320fb3cc3c
andrew bostick <andbostick@gmail.com>
2022-06-25 20:21:51 -0400

    temp search bar functionality

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,49 @@
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import Header from "../components/Header";
-import groq from 'groq'
+import groq from "groq";
 import LargeGrid from "../components/LargeGrid";
 import PostsGrid from "../components/PostsGrid";
 import client from "../client";
-import Link from "next/link";
 import Footer from "../components/Footer";
 
+export default function Home({ posts, title }) {
+  const [search, setSearch] = useState("");
 
+  console.log(posts);
+  let searchedPosts = posts.filter((post) => {
+    return post.title.toLowerCase().includes(search);
+  });
 
-export default function Home({posts,title}) {
-  
-  
+  console.log(searchedPosts);
+
   return (
     <main>
-      <Header title={title}/>
-      
-      <LargeGrid articles={posts} />
-      <section>
-        <PostsGrid articles={posts} />
-       
-      </section>
+      <Header title={title} />
+      <div>
+        <h2>Search through posts</h2>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+
+      {searchedPosts.length ? (
+        <div>
+          <LargeGrid articles={searchedPosts} />
+          <section>
+            <PostsGrid articles={searchedPosts} />
+          </section>
+        </div>
+      ) : (
+        <div>
+          <LargeGrid articles={posts} />
+          <section>
+            <PostsGrid articles={posts} />
+          </section>
+        </div>
+      )}
       <Footer />
       <style jsx>{`
         @media (min-width: 1280px) {
@@ -39,18 +62,17 @@ export default function Home({posts,title}) {
   );
 }
 
-
-
 export async function getStaticProps() {
   const title = await client.fetch(groq`
-  *[_type == 'main']`)
+  *[_type == 'main']`);
   const posts = await client.fetch(groq`
     *[_type == "post" && publishedAt < now()] | order(publishedAt desc)
-  `)
+  `);
+
   return {
     props: {
       posts,
-      title
-    }
-  }
+      title,
+    },
+  };
 }
