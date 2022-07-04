83beb5ea040b7af7132238da0f96abe766b154ec
andrew bostick <andbostick@gmail.com>
2022-07-04 15:59:12 -0400

    youtube player checks if mobile or desktop

--- components/Player.js
+++ components/Player.js
@@ -1,23 +1,41 @@
+import { useState, useEffect } from "react";
 import ReactPlayer from "react-player/youtube";
 
-export default function Player({ytURL}) {
-    
-    return (
-        <div>
-            <ReactPlayer
-            url={ytURL}
-            playing={false}
-                
-                width={'100%'}
-                height={500}
-                config={{
-                    playerVars: {
-                        autoplay: 1,
-                        controls: 1
-                }
-            }}
-            />
-        </div>
-    )
+export default function Player({ ytURL }) {
+  const [isDesktop, setIsDesktop] = useState(false);
 
+  const updateMedia = () => {
+    if (window.innerWidth > 780) {
+      setIsDesktop(true);
+    } else {
+      setIsDesktop(false);
+    }
+  };
+
+  useEffect(() => {
+    if (window.innerWidth > 780) {
+      setIsDesktop(true);
+    } else {
+      setIsDesktop(false);
+    }
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, []);
+  return (
+    <div>
+      <ReactPlayer
+        url={ytURL}
+        playing={false}
+        width={"100%"}
+        height={isDesktop ? 500 : "100%"}
+        config={{
+          playerVars: {
+            autoplay: 1,
+            controls: 1,
+          },
+        }}
+      />
+      
+    </div>
+  );
 }
